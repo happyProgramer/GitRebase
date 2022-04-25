e3da22f48a92daaa4644418c0d3e872077e3c5f3
happyProgramer <15801067062@163.com>
2022-04-25 18:10:09 +0800

    B6 change again

--- GitRebase/GitRebase/ContentView.swift
+++ GitRebase/GitRebase/ContentView.swift
@@ -9,11 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("This is Main update")
-            .padding()
-        Text("This is B1")
-            .padding()
-        Text("This is B2")
         Text("This is B6")
             .padding()
     }
