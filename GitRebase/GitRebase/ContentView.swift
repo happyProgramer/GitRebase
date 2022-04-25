d6ef47370e0d4ff723776c8ffdda30d985b354ff
happyProgramer <15801067062@163.com>
2022-04-25 16:07:31 +0800

    Create repository

---
+++ GitRebase/GitRebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitRebase
+//
+//  Created by yanping han on 2022/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitRebase/GitRebase/GitRebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitRebaseApp.swift
+//  GitRebase
+//
+//  Created by yanping han on 2022/4/25.
+//
+
+import SwiftUI
+
+@main
+struct GitRebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
