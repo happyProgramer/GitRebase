92452dc7aab724c5f41f8b9a72f41d17057f7115
happyProgramer <15801067062@163.com>
2022-04-25 18:03:31 +0800

    Add B6 change

--- GitRebase/GitRebase/ContentView.swift
+++ GitRebase/GitRebase/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     var body: some View {
         Text("This is Main update")
             .padding()
+        Text("This is B1")
+            .padding()
+        Text("This is B2")
+        Text("This is B6")
+            .padding()
     }
 }
 
