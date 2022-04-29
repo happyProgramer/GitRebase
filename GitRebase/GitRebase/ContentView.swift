4c27ced64fed1913b47dabdf7512424b4cf8efc2
happyProgramer <15801067062@163.com>
2022-04-29 12:22:23 +0800

    add main

--- GitRebase/GitRebase/ContentView.swift
+++ GitRebase/GitRebase/ContentView.swift
@@ -9,15 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("This is B6")
-            .padding()
-        Text("This is B5")
-            .padding()
-        Text("This is B4")
-            .padding()
-        Text("This is B3")
-            .padding()
-        Text("This is B2")
+        Text("This is main")
             .padding()
     }
 }
